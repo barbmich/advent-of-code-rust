7312169916ea177ae5f041b29a2559bd629f96cb
Michele Barbiero <barbmich.since86@gmail.com>
2022-12-01 17:23:59 -0500

    revert "create 2022 project"
    This reverts commit e2145c23d7441c7d721ed12a2a81d04d5be8ba60.

--- 2022/target/debug/deps/aoc2022-4a061a1e7fdfa42e.d
+++
@@ -1,5 +0,0 @@
-/Users/michelebarbiero/projects/personal/rust/aoc/2022/target/debug/deps/aoc2022-4a061a1e7fdfa42e: src/main.rs
-
-/Users/michelebarbiero/projects/personal/rust/aoc/2022/target/debug/deps/aoc2022-4a061a1e7fdfa42e.d: src/main.rs
-
-src/main.rs:
